149fc7310e5a0baeb235d0fd963b2c89cddc1cd9
Fabulash1 <wizzlin1993@gmail.com>
2023-07-25 22:06:22 +0100

    learning python

---
+++ python/loops.py
@@ -0,0 +1,49 @@
+# write a program that counts from 0 to 100
+# counter = 0
+# LIMIT = 100
+# # Check for even numbers
+# while counter < LIMIT:
+#     if counter % 2 == 0:
+#         print("{} is even" .format(counter), end="\n")
+#     elif counter % 2 == 1:
+#         print("{} is odd" .format(counter), end="\n")
+#     counter = counter + 1
+# name = "Precious"
+# size = len(name)
+# while counter < size:
+#   print(name[counter].upper(), end="\n")
+#    counter = counter + 1
+# else:
+    # print("loop ends here")
+
+
+    # age = int(input("What is your age?"))
+# height = int(input("How tall are you?"))
+# VOTING_LIMIT = 18 
+# HEIGHT_LIMIT = 10
+# if age > VOTING_LIMIT or height > HEIGHT_LIMIT:
+#     print("You can vote")
+# elif age == VOTING_LIMIT or height == HEIGHT_LIMIT:
+#     print("You are perfect!")
+# else:
+#    print("Sorry, you can't vote")
+
+# FOR LOOP
+# numbers = range(10)
+
+# for number in range(10):
+
+# if number % 2 == 0:
+#     print("{} wow" .format(number))
+# for numbers in "Precious":
+#     print(numbers.upper())
+for i in range(5):
+    for j in range(5):
+        print("{} * {} = {}".format(i,j,(i*j)))
+    # if i <= 30:
+    #     print(i)
+    #     break
+    # else:
+        #print("ended")
+        """i am not sure how this will work out but i will do it anyway"""
+    
